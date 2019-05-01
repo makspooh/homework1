2de4a4c01156f29a5f0a0e39f176d35aa9a6579f
Maksim <maksimpooh@gmail.com>
2019-05-01 17:28:28 +0300

    commit

--- index.js
+++ index.js
@@ -1,159 +1,106 @@
 // task 1
-function storeSale() {
-    var from = +prompt('Введите время начала скидки');
-    var to = +prompt('Введите время окончания скидки');
-    var present = +prompt('Введите текущее время');
+function Car(name, year, distance, color) {
+    this._name = name;
+    this._year = year;
+    this._distance = distance;
+    this._color = color;
+    this._enabled = false;
+    this._run = false;
+    this._gasAmount = 0;
+}
 
-    if(from > to && present < to) {
-        console.log('Скидка действительна ночью');
-    } else if(from < to && present > from) {
-        console.log('Скидка действительна днём');
+Car.prototype.enable = function() {
+    if(this._gasAmount) {
+        this._enabled = true;
+    } else console.log('Нужно заправить автомобиль');
+}
+
+Car.prototype.start = function() {
+    if(this._enabled) {
+        console.log('Машина марки ' + this._name + ' поехала!');
+        this._run = true;
+    } else console.log('Сначала включите зажигание!');
+}
+
+Car.prototype.stop = function() {
+    if(!this._enabled) {
+        console.log('Зажигание и так выключено!')
     } else {
-        console.log('Скидка не действует');
+        this._run = false;
+        this._enabled = false;
+        console.log('Машина остановилась')
     }
 }
 
-storeSale();
+Car.prototype.fillCar = function(value) {
+    this._gasAmount += value;
+}
+
+let ford = new Car('Ford', 2018, 1000, 'Red');
+let ford2 = new Car('Ford2', 2018, 1000, 'Red');
+
+ford2.fillCar(100);
+ford2.enable();
+ford2.start();
+ford2.stop();
+
+ford.fillCar(100);
+ford.enable();
+ford.start();
+ford.stop();
+
+function NewCar() {
+    Car.apply(this, arguments);
+}
+NewCar.prototype = Object.create(Car.prototype);
+NewCar.prototype.constructor = NewCar;
+
+let ford3 = new NewCar('Ford3', 2000, 500000, 'White');
+
+ford3.fillCar(100);
+ford3.enable();
+ford3.start();
+ford3.stop();
 
 // task 2
-function random(min, max) {
-    let rand = min - 0.5 + Math.random() * (max - min + 1);
-    rand = Math.round(rand)
-    console.log(rand)
+function City(name, population, country) {
+    this._name = name;
+    this._population = population;
+    this._country = country;
 }
 
-random(100, 20000)
+City.prototype.getPopulation = function() {
+    return this._population;
+}
+
+City.prototype.getCityName = function() {
+    return this._name;
+}
+
+City.prototype.addCitizen = function() {
+    return this._population += 1;
+}
+
+let kremen = new City('Kremenchug', 200000, 'Ukraine')
+
+console.log(kremen.getPopulation());
+console.log(kremen.getCityName());
+console.log(kremen.addCitizen());
 
 // task 3
-function reverse(str) {
-    let reverseString = '';
+var transport = {
+	stop: null
+};
+var car = {
+	stop: true
+};
+car.__proto__ = transport;
 
-    for(let i = str.length - 1; i >= 0; i--) {
-        reverseString += str[i];
-    }
-    console.log(reverseString);
-}
-
-reverse('Hello world');
-
-// task 4
-function fib(n) {
-    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
-}
-
-console.log(fib(3));
-
-// task 5
-
-function fibo(n) {
-    let a = 1, b = 1;
-    for(let i = 3; i <= n; i++) {
-        let c = a + b;
-        a = b;
-        b = c;
-    }
-    return b;
-}
-
-console.log(fibo(77));
-
-// task 6
-function incCounter() {
-    let counter = 0;
-    return function() {
-        return counter++
-    }
-}
-
-let func1 = incCounter();
-let func2 = incCounter();
-
-console.log(func1());
-console.log(func2());
-console.log(func1());
-console.log(func2());
-
-// task 7
-function multiA(a) {
-    return function(b) {
-       return (a * b);
-    }
-}
-
-let multi3 = multiA(3);
-let multi4 = multiA(4);
-
-console.log(multi3(2));
-console.log(multi4(2));
-
-// task 8
-function getName() {
-    let name = 'Maks';
-    return name;
-}
-
-let obj = {
-    name: getName()
-}
-
-console.log(obj)
-
-// task 9
-let number = null;
-
-function getDoubled(n) {
-    return number = (n + n) * 2;
-}
-
-function getDoubledTrippled(n) {
-    return n * 3;
-}
-
-console.log(getDoubledTrippled.call(this, getDoubled(4)));
-
-// task 10
-let arr = [1, 2, 3, 4, 5];
-
-function random() {
-    return Math.random() - 0.5;
-}
-
-arr.sort(random);
-
-console.log(arr);
-
-// task 11
-let arr = [1, 2, 3, 4, 5];
-
-function sum(a) {
-    let arrSum = null;
-    for (let i = 0; i < a.length; i++) {
-        arrSum += a[i];
-    }
-    return arrSum;
-}
-
-console.log(sum(arr));
-
-// task 12
-let arr = [1, 2, 3, 4, 5];
-
-function max(a) {
-    let maxOfArr = null;
-    for(let i = 0; i < a.length; i++) {
-        a[i] > maxOfArr ? maxOfArr = a[i] : false;
-    }
-    return maxOfArr
-}
-
-console.log(max(arr));
-
-// task 13
-let string = 'оно';
-
-function pal (str) {
-    str = str.toLowerCase().replace(/ /g, '');
-    return str.split('').reverse().join('') == str;
-}
-
-console.log(pal(string));
+console.log(car.stop); // ? (1)
+delete car.stop;
+console.log(car.stop); // ? (2)
+delete transport.stop;
+console.log(car.stop); // ? (3)
+// (1) - true, потому что значение будет браться напрямую из объекта car
+// (2) - null, потому что значение stop было удалено из объекта сar, и далее оно будет найдено в прототипе transport
+// (3) - undefined, потому как значения stop теперь нет ни в одном из объектов
